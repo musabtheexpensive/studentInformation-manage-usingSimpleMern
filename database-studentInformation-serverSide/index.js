ad1d11c381c0f47466a368e78840d6096d901d33
Musab Al Mahi <musabalmahi53@gmail.com>
2024-02-11 17:04:57 +0600

    all functionality was updated

--- database-studentInformation-serverSide/index.js
+++ database-studentInformation-serverSide/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 
